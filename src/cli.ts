2b1db44e96ac6430804b557929ea5cbdfefe5a29
X1 Carbon <yjl9903@vip.qq.com>
2020-03-05 00:18:47 +0800

    add: cli

--- src/cli.ts
+++ src/cli.ts
@@ -3,6 +3,7 @@
 import path from 'path';
 import { readFileSync } from 'fs';
 import { cac } from 'cac';
+import readline from 'readline';
 
 import { LRParser } from './LRparser';
 
@@ -11,11 +12,33 @@ const cli = cac('XParse');
 cli.help();
 
 cli
-  .command('build')
+  .command('build', 'Build LR Parser')
   .option('--config <config>', 'Config file path', { default: 'xparse.config' })
   .action(async (option: { config: string }) => {
     const config = await import(path.resolve(process.cwd(), option.config));
     new LRParser(config);
+  });
+
+cli
+  .command('', 'Run LR Parser')
+  .option('--config <config>', 'Config file path', { default: 'xparse.config' })
+  .action(async (option: { config: string }) => {
+    const config = await import(path.resolve(process.cwd(), option.config));
+    const parser = new LRParser(config);
+
+    const text: string[] = [];
+    process.stdin.on('data', data => {
+      text.push(String(data));
+    });
+    process.stdin.on('end', () => {
+      try {
+        const result = parser.parse(JSON.parse(text.join('')));
+        console.log(result);
+      } catch (err) {
+        console.log(err);
+        process.exit(1);
+      }
+    });
   });
 
 cli.version(
